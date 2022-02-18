a53b5ceb6f17eb5ceaa7297db02022c6056e7392
Сергей Храмцов <vocmarh@mail.ru>
2022-02-18 09:23:09 +0500

    Добавьте файлы проекта.

---
+++ RevitAPILab2_2/Main.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILab2_2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var pipes = new FilteredElementCollector(doc, doc.ActiveView.Id)
+                .OfClass(typeof(Pipe))
+                .Cast<Pipe>()
+                .ToList();
+
+            TaskDialog.Show("Pipes info", pipes.Count.ToString());
+
+            return Result.Succeeded;
+        }
+    }
+}
